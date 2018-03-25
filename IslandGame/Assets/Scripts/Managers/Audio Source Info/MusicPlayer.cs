d0ca34cd3a0e6484eb2a071527012fc88b52b049
RoboChiz <RoboChiz@users.noreply.github.com>
2018-03-25 13:38:39 +0100

    Added Music

---
+++ IslandGame/Assets/Scripts/Managers/Audio Source Info/MusicPlayer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    public AudioClip backgroundMusic;
+
+	// Use this for initialization
+	void Start ()
+    {
+        FindObjectOfType<SoundManager>().PlayMusic(backgroundMusic);
+	}
+}
