c32250cca39a499c62f9a7e364ea4ab58f5b9e9c
RoboChiz <RoboChiz@users.noreply.github.com>
2018-03-26 19:26:41 +0100

    FootSteps

---
+++ IslandGame/Assets/Scripts/Player/FootSteps.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootSteps : MonoBehaviour
+{
+    private GameObject footstepPrefab;
+    public Transform[] feet;
+
+    public float checkDistance = 0.2f, spacing = 0.2f;
+    private Vector3[] lastSpots;
+
+    List<FootStep> steps;
+
+	// Use this for initialization
+	void Start ()
+    {
+        footstepPrefab = Resources.Load<GameObject>("Prefabs/Footprint");
+        steps = new List<FootStep>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(lastSpots == null || lastSpots.Length != feet.Length)
+        {
+            lastSpots = new Vector3[feet.Length];
+        }
+
+        int count = 0;
+		foreach(Transform foot in feet)
+        {
+            RaycastHit hit;
+            if(Physics.Raycast(foot.position, -foot.up, out hit, checkDistance ) && hit.transform.tag == "Sand")
+            {
+                Vector3 spot = hit.point + (hit.normal.normalized * 0.05f);
+                if (Vector3.Distance(spot, lastSpots[count]) > spacing)
+                {
+                    lastSpots[count] = hit.point;
+
+                    GameObject step = Instantiate(footstepPrefab, spot, Quaternion.LookRotation(-hit.normal, transform.forward));
+                    step.GetComponent<MeshRenderer>().material = new Material(step.GetComponent<MeshRenderer>().material);
+                    step.GetComponent<MeshRenderer>().material.color = Color.clear;
+
+                    steps.Add(new FootStep(step));
+                }
+            }
+
+            count++;
+        }
+
+        float diff = FootStep.aliveTime - FootStep.fadeTime;
+        foreach (FootStep step in steps.ToArray())
+        {
+            if (step.timer > 0f)
+            {
+                step.timer -= Time.deltaTime;
+
+                if (step.timer > diff)
+                {
+                    //Fade In
+                    step.prefabClone.GetComponent<MeshRenderer>().material.color = Color.Lerp(Color.clear, Color.white, (FootStep.fadeTime - (step.timer - diff)) / FootStep.fadeTime);
+                }
+                else if (step.timer < FootStep.fadeTime)
+                {
+                    step.prefabClone.GetComponent<MeshRenderer>().material.color = Color.Lerp(Color.white, Color.clear, (FootStep.fadeTime - step.timer) / FootStep.fadeTime);
+                }
+                else
+                {
+                    step.prefabClone.GetComponent<MeshRenderer>().material.color = Color.white;
+                }
+            }
+        }
+    }
+
+    class FootStep
+    {
+        public GameObject prefabClone;
+        public float timer;
+        public const float aliveTime = 5f, fadeTime = 0.5f;
+
+        public FootStep(GameObject _prefab)
+        {
+            prefabClone = _prefab;
+            timer = aliveTime;
+        }
+    }
+}
